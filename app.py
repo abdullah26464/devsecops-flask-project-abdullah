bcc7742a5ad6fa721b5adbbefc20ae15b4cd4fed
Abdullah Waseem <2022-bs-cs-106@tuf.edu.pk>
2025-09-30 16:39:16 +0500

    Update app.py
    Signed-off-by: Abdullah Waseem <2022-bs-cs-106@tuf.edu.pk>

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-app.config['SECRET_KEY'] = 'badsecret123'
 #!/usr/bin/env python3
-
 from flask_bootstrap import Bootstrap5
 from flask_ckeditor import CKEditor
 from flask_login import LoginManager
@@ -8,24 +6,27 @@ from flask import Flask, render_template, render_template_string, request, redir
 from db_seed import setup_db
 from routes import init
 
+# CREATE app FIRST
 app = Flask(__name__)
+
+# THEN configure it
+app.config['SECRET_KEY'] = 'badsecret123'
 app.secret_key = "super secret key"
 app.config["BOOTSTRAP_SERVE_LOCAL"] = True
 app.config["CKEDITOR_SERVE_LOCAL"] = True
+
 bootstrap = Bootstrap5(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 ckeditor = CKEditor()
-
 ckeditor.init_app(app)
+
 init()
 setup_db()
-
 
 @login_manager.unauthorized_handler
 def unauthorized():
     return redirect("/login")
-
 
 @app.errorhandler(404)
 def page_not_found(error):
